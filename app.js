cad0bf52dd8de17c0ab81482ec5e8c98fa08dec8
Thiago Damaglio <Thiago Damaglio>
2024-05-25 20:19:48 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = ('Hora do desafio');
+
+function verificarConsole()
+{
+    console.log ('o botão foi clicado');
+}
+
+function verificarAlert(){
+    alert ('Eu amo JS');
+}
+
+/*
+function verificarPrompt() 
+{
+    let cidade = prompt('Digite uma cidade do Brasil');
+    alert (´Eu estive em ${cidade} e lembre de você´);
+}
+*/
+
